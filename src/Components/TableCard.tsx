b7852071e81e45f50b62b943e38392bd15d46dc5
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-07-09 14:34:59 +0700

    6th

--- src/Components/TableCard.tsx
+++ src/Components/TableCard.tsx
@@ -1,5 +1,6 @@
-import { GlobalContext, TableType } from "@/Hooks/GlobalContext";
-import React, { useContext } from "react";
+import { TableType } from "@/Hooks/GlobalContext";
+import { FaSquareArrowUpRight } from "react-icons/fa6";
+import { VscDebugRestart } from "react-icons/vsc";
 
 export const PageLayout = ({ children }: React.PropsWithChildren) => {
   return (
@@ -7,10 +8,10 @@ export const PageLayout = ({ children }: React.PropsWithChildren) => {
       style={{
         display: "flex",
         flexWrap: "wrap",
-        maxWidth: "1000px",
+        maxWidth: "1280px",
         margin: "0 auto",
         backgroundColor: "#D6DAA5",
-        padding: "15px",
+
         columnGap: "12px",
         rowGap: "10px",
         justifyContent: "center",
@@ -27,13 +28,9 @@ export const TableContainer = ({ children }: React.PropsWithChildren) => {
     <div
       style={{
         backgroundColor: "white",
-        width: "280px",
-        height: "180px",
-        border: "1px solid blue",
+        border: "1px solid #d9d9d9",
         borderRadius: "10px",
-        display: "flex",
-        flexWrap: "wrap",
-        position: "relative",
+        padding: "10px",
       }}
     >
       {children}
@@ -53,7 +50,8 @@ export const TableNumber = ({ no }: { no: number }) => {
           position: "absolute",
           transform: "translateY(-50%)",
           alignItems: "center",
-          left: "110px",
+          left: "100px",
+          top: "-25px",
         }}
         src="/lable.png"
         width={"120px"}
@@ -65,7 +63,8 @@ export const TableNumber = ({ no }: { no: number }) => {
           position: "absolute",
           fontSize: "30px",
           transform: "translateY(-50%)",
-          left: "140px",
+          left: "125px",
+          top: "-25px",
           color: "white",
           fontWeight: "bold",
         }}
@@ -78,40 +77,62 @@ export const TableNumber = ({ no }: { no: number }) => {
 export const OrderList = ({ table: { no, cart } }: { table: TableType }) => {
   const statusDone = {
     backgroundColor: "green",
+    color: "white",
   };
   const statusPanding = {
     backgroundColor: "yellow",
   };
 
   return (
-    <>
+    <div style={{ marginTop: "50px" }}>
       <TableNumber no={no} />
       <div
         style={{
-          position: "absolute",
-          left: "50px",
-          display: "flex",
-          flexDirection: "column",
+          display: "grid",
+          gap: "10px",
+          justifyContent: "start",
         }}
       >
-        {cart.map((item) => (
+        {cart.map((item, index) => (
           <div
             style={{
-              alignItems: "center",
-              border: "1px solid black",
+              border: "1px solid #FBF8EE",
               display: "flex",
+              position: "relative",
+              justifyContent: "center",
+              borderRadius: "10px",
+              padding: "5px",
+              alignItems: "center",
               ...(item.status === "done" ? statusDone : statusPanding),
             }}
           >
+            <p style={{ marginRight: "10px", fontSize: "20px" }}>{index + 1}</p>
             <img src={item.image} width={40} />{" "}
             <span style={{ marginLeft: "10px" }}></span>
-            <p style={{ marginRight: "10px" }}>
+            <p style={{ marginRight: "10px", fontSize: "22px" }}>
               {item.name}{" "}
-              <span style={{ marginLeft: "10px" }}>{item.amount}</span>
+              <span
+                style={{
+                  marginLeft: "10px",
+                }}
+              >
+                จำนวน : {item.amount}
+              </span>
+              <span style={{ marginLeft: "15px" }}>
+                {item.status === "done" ? (
+                  <VscDebugRestart
+                    style={{ fontSize: "30px", marginBottom: "-10px" }}
+                  />
+                ) : (
+                  <FaSquareArrowUpRight
+                    style={{ fontSize: "30px", marginBottom: "-10px" }}
+                  />
+                )}
+              </span>
             </p>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
